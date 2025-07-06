e00637e51e3bfc0aa7630fcbfd15cae271c62876
au693591 <202106964@post.au.dk>
2025-07-06 19:42:20 +0200

    Created BurgerMenuButton component

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,15 +1,13 @@
 <script setup lang="ts">
 import AvatarCircle from './AvatarCircle.vue';
-import IconBurgerMenu from './icons/IconBurgerMenu.vue';
+import BurgerMenuButton from './BurgerMenuButton.vue';
 import placeholderHandler from '@/utils/placeholderHandler';
 </script>
 
 <template>
   <div class="container">
     <div class="row" style="align-items: center; gap: 0.5rem;">
-      <button class="burger-menu" @click="placeholderHandler('Burger-menu button')">
-        <IconBurgerMenu />
-      </button>
+      <BurgerMenuButton @click="placeholderHandler('Open site navigation menu')"/>
       <a class="brand" href="/">
         <img class="logo" src="@/assets/loldle_logo_black.svg" />
         <div>LoLdle</div>
@@ -26,24 +24,6 @@ import placeholderHandler from '@/utils/placeholderHandler';
   align-items: center;
   justify-content: space-between;
   width: 100%;
-}
-
-.burger-menu {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: var(--color-background);
-  width: 1.8rem;
-  height: 1.8rem;
-  padding: 0.2rem;
-  border: 1px;
-  border-style: solid;
-  border-radius: 25%;
-  cursor: pointer;
-}
-
-.burger-menu:hover {
-  background-color: var(--color-background-mute);
 }
 
 .brand {
---
+++ src/components/BurgerMenuButton.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import IconBurgerMenu from './icons/IconBurgerMenu.vue';
+
+const emit = defineEmits<{
+  (emit: "click", payload: MouseEvent): void
+}>();
+</script>
+
+<template>
+  <button class="burger-menu" @click="event => emit('click', event)">
+    <IconBurgerMenu />
+  </button>
+</template>
+
+<style scoped>
+.burger-menu {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: var(--color-background);
+  width: 1.8rem;
+  height: 1.8rem;
+  padding: 0.2rem;
+  border: 1px;
+  border-style: solid;
+  border-radius: 25%;
+  cursor: pointer;
+}
+
+.burger-menu:hover {
+  background-color: var(--color-background-mute);
+}
+</style>
