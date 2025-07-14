28ee37246756f366d458097905b72777071dfcc2
au693591 <202106964@post.au.dk>
2025-07-14 21:32:48 +0200

    Added source collection path

--- frontend/src/components/icons/IconCross.vue
+++ frontend/src/components/icons/IconCross.vue
@@ -1,3 +1,4 @@
+<!-- From collection: https://www.svgrepo.com/collection/ui-oval-interface-icons/ -->
 <template>
   <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor">
     <path d="M17 7L7 17M7 7L17 17" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
--- frontend/src/components/icons/IconHome.vue
+++ frontend/src/components/icons/IconHome.vue
@@ -1,3 +1,4 @@
+<!-- From collection: https://www.svgrepo.com/collection/ui-oval-interface-icons/ -->
 <template>
   <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor">
     <path
