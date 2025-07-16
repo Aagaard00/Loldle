80a4ff670a8249df2f9a0374a52f53d2cfb4d9ac
au693591 <202106964@post.au.dk>
2025-07-16 19:40:51 +0200

    implemented dynamic title

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,42 +1,48 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
+const baseName: string = "Loldle";
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      name: "Home",
+      name: `${baseName}`,
       // Eager-loaded when app starts
       component: HomeView,
     },
     {
       path: "/classic",
-      name: "Classic",
+      name: `Classic | ${baseName}`,
       // Lazy-loaded when the route is visited.
       component: () => import("../views/ClassicGameView.vue"),
     },
     {
       path: "/quote",
-      name: "Quote",
+      name: `Quote | ${baseName}` ,
       component: () => import("../views/QuoteGameView.vue"),
     },
     {
       path: "/ability",
-      name: "Ability",
+      name: `Ability | ${baseName}`,
       component: () => import("../views/AbilityGameView.vue"),
     },
     {
       path: "/emoji",
-      name: "Emoji",
+      name: `Emoji | ${baseName}`,
       component: () => import("../views/EmojiGameView.vue"),
     },
     {
       path: "/splash",
-      name: "Splash",
+      name: `Splash | ${baseName}`,
       component: () => import("../views/SplashGameView.vue"),
     },
   ],
+});
+
+router.beforeEach((to, from) => {
+  document.title = to.name?.toString() ?? baseName;
 });
 
 export default router;
